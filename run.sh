654aa508817467ec4a7ecbe9c6c03113e690a128
Tien Dung <dungtn@gmail.com>
2022-12-13 13:19:37 +0700

    gpu.py -> gpu.codon

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # sudo apt-get install python3-tk
+codon/build/codon run -release fib.py
 codon/build/codon run -release man.codon
-# codon/build/codon run -release fib.py
-# codon/build/codon run -release gpu.py
+# codon/build/codon run -release gpu.codon
